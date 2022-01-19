8faa7f8feae38053517b6338ae21d28cfe10e1c4
Jingyu Song <jingyu_song@outlook.com>
2022-01-19 14:34:27 -0500

    upload recent work (change config, to spconv2)

--- prep_carla.py
+++ prep_carla.py
@@ -1,14 +1,14 @@
 import os
 import yaml
 
-raw_dataset_dir = '/media/neofelis/c6edf27e-8489-4fb6-af1c-2530dc1ff67b/jy/carla_data'
+raw_dataset_dir = '/media/sdb1/carla_dataset'
 dataset_dir = './carla_data'
 sequence_dir = os.path.join(dataset_dir, 'sequences')
 sequence_ref_dict = {}
-is_cylindrical = True
+is_cylindrical = False
 sequence_ref_dict['is_cylindrical'] = is_cylindrical
 
-
+train_val_test_split = []
 
 
 os.system(f'ls {raw_dataset_dir}')
@@ -31,7 +31,31 @@ for i in range(len(sequences)):
     os.system(f'ln -s {raw_dataset_dir}/Train/{sequences[i]}/{coordinate} {os.path.join(sequence_dir, str(i).zfill(2))}')
     # generate soft links
 
+train_val_test_split.append(len(sequences))
+len_train = len(sequences)
+
+sequences = sorted(os.listdir(os.path.join(raw_dataset_dir, 'Val')))
+for i in range(len(sequences)):
+    sequence_ref_dict[i+len_train] = sequences[i]
+    
+    os.system(f'ln -s {raw_dataset_dir}/Val/{sequences[i]}/{coordinate} {os.path.join(sequence_dir, str(i+len_train).zfill(2))}')
+    # generate soft links
+
+len_train_val = len_train + len(sequences)
+train_val_test_split.append(len(sequences))
+
+
+sequences = sorted(os.listdir(os.path.join(raw_dataset_dir, 'Test')))
+for i in range(len(sequences)):
+    sequence_ref_dict[i+len_train_val] = sequences[i]
+    
+    os.system(f'ln -s {raw_dataset_dir}/Test/{sequences[i]}/{coordinate} {os.path.join(sequence_dir, str(i+len_train_val).zfill(2))}')
+    # generate soft links
+train_val_test_split.append(len(sequences))
+
+sequence_ref_dict['split'] = train_val_test_split
 
 print(sequence_ref_dict)
+
 with open(os.path.join(dataset_dir, 'carla_seq.yaml'), 'w') as outfile:
     yaml.dump(sequence_ref_dict, outfile)
